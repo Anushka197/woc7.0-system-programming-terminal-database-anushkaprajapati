c6762e7111c5dd4e3b2a9d62b00157da1da11043
Anushka197 <159715874+Anushka197@users.noreply.github.com>
2025-01-22 16:48:07 +0530

    Update t1_server.py
    Used JSON file to get the data

--- Task1/t1_server.py
+++ Task1/t1_server.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
@@ -7,16 +8,13 @@ port = 1234
 server_socket.bind((host, port))
 server_socket.listen(1)
 
-int_str_dictionary = {
-    1 : "NLP",
-    2 : "System Programming",
-    3 : "Django"
-}
+with open("dictionary.json", "r") as file:
+    int_str_dictionary = json.load(file)
+
 while True:
     client_socket, client_addr = server_socket.accept()
-    message = client_socket.recv(1024).decode()
+    key = client_socket.recv(1024).decode()
     try:
-        key = int(message)
         print(f"Received key: {key}")
     except ValueError:
         print("Invalid input. Please send an integer.")
